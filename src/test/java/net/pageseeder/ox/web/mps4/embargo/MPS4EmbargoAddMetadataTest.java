40885bbb1a0039956d9ca8b70e04534b71b5c751
ccabral <ccabral@allette.com.au>
2023-01-10 21:16:00 +1100

    add first step for new pipeline "add metadata", extract data.

---
+++ src/test/java/net/pageseeder/ox/web/mps4/embargo/MPS4EmbargoAddMetadataTest.java
@@ -0,0 +1,73 @@
+/* Copyright (c) 2021 Allette Systems pty. ltd. */
+package net.pageseeder.ox.web.mps4.embargo;
+
+import net.pageseeder.ox.web.mps4.MPS4OXTest;
+import org.pageseeder.ox.OXConfig;
+import org.pageseeder.ox.api.Result;
+import org.pageseeder.ox.core.Model;
+import org.pageseeder.ox.core.Pipeline;
+import org.pageseeder.ox.step.StepSimulator;
+import org.pageseeder.ox.xml.utils.XMLComparator;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Carlos Cabral
+ * @since 06 December 20222
+ */
+public abstract class MPS4EmbargoAddMetadataTest extends MPS4OXTest {
+
+
+  protected final static String MODEL_NAME = "embargo";
+  protected final static String PIPELINE_NAME = "embargo-add-metadata";
+
+
+  private static Model MODEL = null;
+  private static Pipeline PIPELINE = null;
+
+  public static void init () {
+    File modelDir = new File("local/appdata/model");
+    OXConfig config = OXConfig.get();
+    config.setModelsDirectory(modelDir);
+    MODEL = new Model(MPS4EmbargoAddMetadataTest.MODEL_NAME);
+    MODEL.load();
+    PIPELINE = MODEL.getPipeline(MPS4EmbargoAddMetadataTest.PIPELINE_NAME);
+  }
+
+  protected static Map<String, String> getDefaultRequestParameters() {
+    Map<String, String> requestParameter = new HashMap<>();
+    return requestParameter;
+  }
+
+  protected Result performExtractDataFromSpreadsheet(StepSimulator simulator) {
+    return performGeneric(simulator, "extract-data-from-spreadsheet");
+  }
+
+  protected Result performValidateExtractedData(StepSimulator simulator) {
+    return performGeneric(simulator, "validate-extracted-data");
+  }
+
+  protected Result performAddMetadatas(StepSimulator simulator) {
+    return performGeneric(simulator, "add-metadatas");
+  }
+
+  protected Result performXMLToCSV(StepSimulator simulator) {
+    return performGeneric(simulator, "xml2csv");
+  }
+
+  protected void compareActualExpected(File expectedResultBase, String folderOrFileName, StepSimulator simulator) {
+    File actualResultBase = new File(simulator.getData().directory(), folderOrFileName);
+    XMLComparator.compareXMLFile(expectedResultBase, actualResultBase);
+  }
+
+  protected Model getModel() {
+    return MODEL;
+  }
+
+  protected Pipeline getPipeline() {
+    return PIPELINE;
+  }
+}
---
+++ src/test/java/net/pageseeder/ox/web/mps4/embargo/MSP4EmbargoAddMetadata_performExtractDataTest.java
@@ -0,0 +1,51 @@
+package net.pageseeder.ox.web.mps4.embargo;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.pageseeder.ox.api.Result;
+import org.pageseeder.ox.core.ResultStatus;
+import org.pageseeder.ox.step.StepSimulator;
+import org.pageseeder.xmlwriter.XML;
+import org.pageseeder.xmlwriter.XMLStringWriter;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * @author vku
+ * @since 04 November 2021
+ */
+public class MSP4EmbargoAddMetadata_performExtractDataTest extends MPS4EmbargoAddMetadataTest {
+  private static Map<String, String> REQUEST_PARAMETER = null;
+
+  @BeforeClass
+  public static void init () {
+    MPS4EmbargoAddMetadataTest.init();
+    REQUEST_PARAMETER = MPS4EmbargoAddMetadataTest.getDefaultRequestParameters();
+  }
+
+  @Test
+  public void performExtractData() {
+    try {
+      File input = new File("src/test/resources/net/pageseeder/ox/web/mps4/addmetadata/basic/embargo-add-metadata-test.xlsx");
+      StepSimulator simulator = new StepSimulator(super.getModel().name(), input, REQUEST_PARAMETER);
+      Result result = super.performExtractDataFromSpreadsheet(simulator);
+      if (!ResultStatus.OK.equals(result.status())) {
+        XMLStringWriter writer = new XMLStringWriter(XML.NamespaceAware.No);
+        result.toXML(writer);
+//        System.out.println("Result");
+//        System.out.println(writer);
+      }
+      Assert.assertEquals(ResultStatus.OK, result.status());
+    } catch (Exception ex) {
+      Assert.fail(ex.getMessage());
+    }
+  }
+
+  @Override
+  protected File getExpectedResultDirectory() {
+    return null;
+  }
+}
+
